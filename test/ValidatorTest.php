99c70444bcc88c92489f93d3c5789a5eb3d32c96
raph <rap2hpoutre@users.noreply.github.com>
2016-06-03 15:16:50 +0200

    Create ValidatorTest.php

---
+++ test/ValidatorTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Rap2hpoutre\LaravelCreditCardValidator\Test;
+/**
+ * Class JackyTest
+ * @package Rap2hpoutre\LaravelCreditCardValidator\Test
+ */
+class ValidatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testStupid() 
+    {
+        // Todo.
+        $this->assertTrue(true);
+    }
+}
